7a5fd8b3230f8eafb659c2e9361b33d912854fcc
AnthonyVergottis <a.vergottis@gmail.com>
2020-02-04 17:36:21 +0000

    Mergesort added

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -1,27 +1,13 @@
-/*
+#include <iostream>
 
-    Implementation of Merge Sort Algorithm
-
-        - Average performance runtime O(n log n)
-
-    Notes:
-       -
-    
-    
-    
-*/
+using namespace std;
 
 int main(){
-
-
-    int arr[] =  {5,76,3,56,7,53,1,4,8,2,500,23,555,677,422,44};
+    cout << "hello" << endl;
     
-    
-    
-    
-    
-    
-    
+    int arr[] = {12, 11, 13, 5, 6, 7};
     
     return 0;
+
+
 }
