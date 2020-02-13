3f1e8c645c1b37dd539433333270dd34eb261a65
AnthonyVergottis <a.vergottis@gmail.com>
2020-02-13 14:44:12 +0000

    Test

--- Graphs/shortestDistanceAlgorithm.cpp
+++ Graphs/shortestDistanceAlgorithm.cpp
@@ -8,20 +8,29 @@
 
 #include <iostream>
 #include <utility>
+#include <limits.h> 
+#include <stdio.h> 
 
 using namespace std;
 
+// Graph contains 9 points
 static const int SIZE = 9;
 
-void computeMinDistance(){
+void computeMinDistance(int distArray[], bool sptSet[]){
     for(int i = 0; i < SIZE; ++i){
 
     }
 }
 
 
-void dijkstrasAlgo(int graph[SIZE][SIZE]){
+void shortestDistanceAlgorithm(int graph[SIZE][SIZE], int src){
 
+    int dist[SIZE] = {INT_MAX};
+    int shortedPathSet[SIZE] = {false};
+
+    dist[src] = 0;
+
+    
 }
 
 
@@ -29,18 +38,19 @@ int main(){
     // Graph Structure taken from (https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/)
 
     int src_vertex  = 0;
-    int dest_vertex = 0;
+    // int dest_vertex = 0;
+    // cout << "Enter destination vertex: " << endl;
+    // cin >> dest_vertex;
 
-    cout << "Enter source vertex: " << endl;
+    cout << "Enter source vertex, " << "index must be between 0-"<< SIZE-1 << "." << endl;
     cin >> src_vertex;
+    if (!(src_vertex >= 0 && src_vertex < SIZE)){
+        cout << "\nSource vertex value is not within range!!. Try again.\n" << endl;
+        return 0;
+    }
+    cout << "\nComputing the shortest distance between source vertex and all other vertices in graph!!\n" << endl;
 
-    cout << "Enter destination vertex: " << endl;
-    cin >> dest_vertex;
-
-
-
-    int dist[8] = {0};
-
+    
     int graph[SIZE][SIZE] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
                               { 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
                               { 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
@@ -52,7 +62,10 @@ int main(){
                               { 0, 0, 2, 0, 0, 0, 6, 7, 0 } }; 
 
 
-
+    shortestDistanceAlgorithm(graph,0);
     return 0;
 }
+
+
+  
 
